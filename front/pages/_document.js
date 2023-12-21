aebd4eb4a3b64a151fdd73d1fff9426e1f9bfb92
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2023-12-21 16:01:30 +0900

    css 서버사이드 렌더링 구현

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -1,39 +1,45 @@
-// _document.tsx
-import { createCache, StyleProvider } from "@ant-design/cssinjs";
-
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
 
 export default class MyDocument extends Document {
+  // eslint-disable-next-line consistent-return
   static async getInitialProps(ctx) {
-    const cache = createCache();
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          (
+    const cache = createCache();
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(
             <StyleProvider cache={cache}>
               <App {...props} />
             </StyleProvider>
           ),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    // 1.1 extract style which had been used
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {/* 1.2 inject css */}
-        </>
-      ),
-    };
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      const style = extractStyle(cache, true);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+            <style dangerouslySetInnerHTML={{ __html: style }} />
+          </>
+        ),
+      };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head />
         <body>
           <Main />
